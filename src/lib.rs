dcac1f78f3e385762650f9521377c53df9ae0bc5
Titus Tienaah <titus.intdxdt@gmail.com>
2019-11-09 21:36:22 +0000

    updated params

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use robust_sum::robust_sum as rsum;
 use robust_scale::robust_scale as rscale;
 use two_product::two_product as tprod;
 
-pub fn det2(m: &[Vec<f64>]) -> Vec<f64> {
+pub fn det2(m: &[[f64;2]]) -> Vec<f64> {
     let mut e = rsum(
         &tprod(m[0][0], m[1][1]),
         &tprod(-m[0][1], m[1][0])
@@ -16,7 +16,7 @@ pub fn det2(m: &[Vec<f64>]) -> Vec<f64> {
     compress(&mut e)
 }
 
-pub fn det3(m: &[Vec<f64>]) -> Vec<f64> {
+pub fn det3(m: &[[f64;3]]) -> Vec<f64> {
     let mut e = rsum(
         &rscale(&rsum(
             &tprod(m[1][1], m[2][2]),
@@ -45,9 +45,9 @@ mod test_det {
 
     #[test]
     fn test_robust_det() {
-        assert_eq!(det3(&vec!(vec!(1., 2., 3.), vec!(3., 4., 5.), vec!(6., 7., 8.))), [0.]);
-        assert_eq!(det2(&vec!(vec!(1., 2.), vec!(3., 4.))), [-2.]);
-        assert_eq!(det3(&vec!(vec!(1., 2., 3.), vec!(3., 4., 5.), vec!(6., 7., 8.))), [0.]);
-        assert_eq!(det2(&vec!(vec!(1., 2.), vec!(3., 4.))), [-2.]);
+        assert_eq!(det3(&[[1., 2., 3.], [3., 4., 5.], [6., 7., 8.]]), [0.]);
+        assert_eq!(det2(&[[1., 2.], [3., 4.]]), [-2.]);
+        assert_eq!(det3(&[[1., 2., 3.], [3., 4., 5.], [6., 7., 8.]]), [0.]);
+        assert_eq!(det2(&[[1., 2.], [3., 4.]]), [-2.]);
     }
 }
